c159dec8a8e11c2015b99526cc9a771d946124f5
Ruud van Asseldonk <dev@veniogames.com>
2016-06-26 20:37:16 +0200

    Simplify parsing branches from git-for-each-ref

--- src/Git.hs
+++ src/Git.hs
@@ -6,7 +6,6 @@
 
 module Git (listBranches) where
 
-import Data.Char (isSpace)
 import Data.List (stripPrefix)
 import Data.Maybe (fromJust)
 
@@ -26,16 +25,8 @@ instance Show Sha where
 instance Show Branch where
   show (Branch branch) = branch
 
--- Parses the output of `git for-each-ref refs/heads`.
-parseBranches :: String -> [Branch]
-parseBranches raw = map takeBranch $ lines raw
-  where takeBranch line =
-          let strippedSha  = tail $ dropWhile (not . isSpace) line
-              strippedType = tail $ dropWhile (not . isSpace) strippedSha
-              strippedRef  = stripPrefix "refs/heads/" strippedType
-          in  Branch $ fromJust strippedRef
-
 listBranches :: GitOperation [Branch]
 listBranches = do
-  branches <- runGitCommand ["for-each-ref", "refs/heads"]
-  return $ parseBranches branches
+  branches <- runGitCommand ["for-each-ref", "--format=%(refname)", "refs/heads"]
+  return $ fmap parseBranch $ lines branches
+    where parseBranch = Branch . fromJust . stripPrefix "refs/heads/"
